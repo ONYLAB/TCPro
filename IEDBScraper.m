04d6c5e66c00257c97321fc6304470117572715f
Osman <yogurtcuon@gmail.com>
2018-01-02 11:27:08 -0500

    IEDB error handling
    for web connections

--- IEDBScraper.m
+++ IEDBScraper.m
@@ -1,4 +1,4 @@
-function kon = IEDBScraper(peptidesequence,nallele,allelelist)
+function rank = IEDBScraper(peptidesequence,nallele,allelelist)
 
 url = 'http://tools-cluster-interface.iedb.org/tools_api/mhcii/';
 method = 'method';
@@ -12,21 +12,35 @@ for i = 1:nallele
         alleleSelect = [alleleSelect ','];
     end
     al = allelelist{i};
-    alleleSelect = [alleleSelect 'HLA-' al(1:4) '*' al(6:7) ':' al(8:9)];
+    alleleSelect = [alleleSelect al];
     % alleleSelect = 'HLA-DRB1*10:01,HLA-DRB1*12:01';
 end
 
-
-response = webwrite(url,method,methodSelect,sequence_text,sequence_textSelect,allele,alleleSelect);
+count = 0;
+err_count = 0;
+while count == err_count
+    try
+        response = webwrite(url,method,methodSelect,sequence_text,sequence_textSelect,allele,alleleSelect,weboptions('Timeout',24*60*60));
+    catch MyErr
+        err_count = err_count + 1;
+        disp('IEDB Connection failed, retrying in 1 minute');
+        pause(60);
+    end
+    count = count + 1;
+end
 
 fileID = fopen('IEDBresult.dat','w');
 nbytes = fprintf(fileID,'%c',response);
 fclose(fileID);
 
-res = readtable('IEDBresult.dat');
+res = readtable('IEDBresult.dat','Delimiter','\t');
 for i = 1:nallele
     temp = res(strcmp(res.allele,allelelist{i}),'percentile_rank');
-    rank(i,1) = temp.mean_percentile_rank;
+    rank(i,1) = temp.percentile_rank;
+end
+
+if nallele == 1
+    rank(2,1) = rank(1,1); %Homozygote check
 end
 
 % convert rank to affinity
