21354cbed388dc6cfce8269cc3dddc7b84e98c0f
Osman <yogurtcuon@gmail.com>
2018-01-10 08:59:37 -0500

    correction

--- optimizationdriver.m
+++ optimizationdriver.m
@@ -3,7 +3,7 @@ function optimizationdriver()
 RhoNT=0.1432; % day-1
 
 %BetaNT: death rate of naive helper T cells
-BetaNT=0.3048;%%PREVIOUSLY:0.018; % day-1
+RhoAT=1.5;%%PREVIOUSLY:0.018; % day-1
 
 %BetaAT: death rate of activated helper T cells
 BetaAT=0.18; % day-1
@@ -12,10 +12,10 @@ A = [];
 b = [];
 Aeq = [];
 beq = [];
-lb = [0.1 0.033 0.13];
-ub = [0.2963 0.5765 0.23];
+lb = [0.1 0.5973 0.13];
+ub = [0.2963 1.6 0.23];
 nonlcon = [];
-x0 = [RhoNT,BetaNT,BetaAT];
+x0 = [RhoNT,RhoAT,BetaAT];
 
 options = optimoptions('fmincon','Display','iter');
 
