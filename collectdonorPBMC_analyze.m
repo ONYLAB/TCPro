6bae99c5fa684af58db0f636a57874e0ded4d36c
Osman <yogurtcuon@gmail.com>
2018-01-16 14:04:12 -0500

    Updates
    Freeze donor num cells
    Print num cells

--- collectdonorPBMC_analyze.m
+++ collectdonorPBMC_analyze.m
@@ -10,6 +10,21 @@ CD4 = Vp*NumCellsper_ml(4);
 CD8 = Vp*NumCellsper_ml(5);
 MC = Vp*NumCellsper_ml(6);
 
+if ~exist('InitNumCell.txt')
+    PBMCcells{1}='DC: ';
+    PBMCcells{2}='NK: ';
+    PBMCcells{3}='BC: ';
+    PBMCcells{4}='CD4: ';
+    PBMCcells{5}='CD8: ';
+    PBMCcells{6}='MC: ';
+    fileID = fopen('InitNumCell.txt','w');
+    fprintf(fileID,'%s\n','Init Cell Counts');
+    for i = 1:6
+        fprintf(fileID,'%s %d\n', PBMCcells{i}, Vp*NumCellsper_ml(i));
+    end
+    fclose(fileID);
+end
+
 % disp('Init Cell Counts');
 % disp(['DC: ' num2str(round(DC))]);
 % disp(['NK: ' num2str(round(NK))]);
